fed3ee03e97e27298edec1f087bd11d4891781f3
Maximilian Tagher <feedback.tagher@gmail.com>
2014-12-13 19:43:59 -0800

    Explained areq

--- Handler/PostNew.hs
+++ Handler/PostNew.hs
@@ -1,8 +1,18 @@
 module Handler.PostNew where
 
 import Import
+import Yesod.Form.Bootstrap3
+
+data BlogPost = BlogPost
+        { title :: Text
+        }
+
+blogPostForm :: AForm Handler BlogPost
+blogPostForm = BlogPost 
+            <$> areq textField (bfs ("Title" :: Text)) Nothing
 
 getPostNewR :: Handler Html
 getPostNewR = do
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
     defaultLayout $ do
         $(widgetFile "posts/new")
