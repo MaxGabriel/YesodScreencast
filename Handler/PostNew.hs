3000a4fae066b64c6ba0828f7c67dfb22882a0eb
Maximilian Tagher <feedback.tagher@gmail.com>
2014-12-14 17:43:32 -0800

    Article property supported

--- Handler/PostNew.hs
+++ Handler/PostNew.hs
@@ -2,14 +2,18 @@ module Handler.PostNew where
 
 import Import
 import Yesod.Form.Bootstrap3
+import Text.Markdown (Markdown)
+import Yesod.Text.Markdown
 
 data BlogPost = BlogPost
         { title :: Text
+        , article :: Markdown
         }
 
 blogPostForm :: AForm Handler BlogPost
 blogPostForm = BlogPost 
             <$> areq textField (bfs ("Title" :: Text)) Nothing
+            <*> areq markdownField (bfs ("Article" :: Text)) Nothing
 
 getPostNewR :: Handler Html
 getPostNewR = do
