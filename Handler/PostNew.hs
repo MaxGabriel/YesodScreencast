f7c7be79f9256e271ac3aa9371f672737dc020df
Maximilian Tagher <feedback.tagher@gmail.com>
2015-01-03 17:41:14 -0800

    Add redirect

--- Handler/PostNew.hs
+++ Handler/PostNew.hs
@@ -20,7 +20,7 @@ postPostNewR = do
     ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
     case res of
       FormSuccess blogPost -> do
-              _ <- runDB $ insert blogPost
-              error "todo"
+              blogPostId <- runDB $ insert blogPost
+              redirect $ PostDetailsR blogPostId
       _ -> defaultLayout $(widgetFile "posts/new")
 
